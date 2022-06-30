aa212c451292469fb42243eb7d47b1a84226cfc6
cuongdoan <cuongdoan.brilliant@gmail.com>
2022-06-30 21:51:51 +0700

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CdkTableModule} from '@angular/cdk/table';
+import {MatTableModule} from '@angular/material/table'; 
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    CdkTableModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/contract.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContractService {
+
+  baseUrl = environment.baseUrl;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+
+
+  getInfoTransactions(body: any): Observable<any> {
+    return this.http.post(this.baseUrl + '/contracts/search-transactions', body);
+  }
+
+
+}
