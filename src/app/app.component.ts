d861f71570c9e5ca56eade2811d45bd0f580ff02
cuongdoan <cuongdoan.brilliant@gmail.com>
2022-06-30 21:51:51 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContractService } from './services/contract.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'jobchains';
+  searchWord: string | undefined;
+  dataSource: any;
+
+  displayedColumns: string[] = ['tx_hash', 'blockId', 'method'];
+
+
+  constructor(private contractService: ContractService) {
+  }
+
+
+  ngOnInit(): void {
+    
+
+  }
+
+  searchInfo() {
+    let body = 
+    {
+      "limit": 25,
+      "offset": 0,
+      "label": "",
+      "contract_address": this.searchWord
+    };
+
+    this.contractService.getInfoTransactions(body).subscribe((value) => {
+
+      console.log(value);
+      
+      this.dataSource = value.data;
+    });
+  }
+
+// aura1eq9uvjm5l9p3a8qwv9z0asxy494wu0uy7mrcw9vjke0p0a2ml8aqjqqxz4
+
+
+
+
+
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl:  'https://serenity-api.aurascan.io/api/v1'
 };
